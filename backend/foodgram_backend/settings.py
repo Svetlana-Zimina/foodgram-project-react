37886f37e0eeb6f2a743f82e2d6cd10393b4302b
Светлана Зимина <Zima271985@yandex.ru>
2023-08-31 08:50:50 +0300

    Настройка статики

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
+STATIC_ROOT = BASE_DIR / 'backend_static/static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / '/media'
