96ac5f738b5db86062c9cde2947aa6815d26ef03
Светлана Зимина <Zima271985@yandex.ru>
2023-08-31 09:32:47 +0300

    Корректровка settings

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -1,3 +1,4 @@
+# flake8: noqa
 import os
 from pathlib import Path
 
@@ -10,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEBUG', False) == True
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
