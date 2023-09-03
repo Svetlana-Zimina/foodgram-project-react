5365586e518ca20e6ade6f988d7041de9a5ff10a
Светлана Зимина <Zima271985@yandex.ru>
2023-09-03 09:06:00 +0300

    правки flake8

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -1,7 +1,6 @@
 import base64
 
 from django.core.files.base import ContentFile
-from django.db.models import F
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
