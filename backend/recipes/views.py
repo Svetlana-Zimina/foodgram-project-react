a55651a28626874d6026c56da82091c6ddfd79fd
Светлана Зимина <Zima271985@yandex.ru>
2023-09-04 12:32:00 +0300

    isort

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -2,7 +2,7 @@ from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import exceptions, filters, status, permissions, viewsets
+from rest_framework import exceptions, filters, permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from users.serializers import RecipeShortSerializer
@@ -11,7 +11,7 @@ from .filters import RecipeFilter
 from .models import (Favorite, Ingredient, IngredientRecipe, Recipe,
                      ShoppingCart, Tag)
 from .pagination import CustomPagination
-from .permissions import (AdminOrReadOnly, IsAuthorPermission)
+from .permissions import AdminOrReadOnly, IsAuthorPermission
 from .serializers import (IngredientSerializer, RecipeListSerializer,
                           RecipeSerializer, TagSerializer)
 
